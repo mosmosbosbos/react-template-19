f5c3c9f35c6fbf3f5f38d71ab537109d5ac6bf69
MohamedAlghurbani <mohamedfadl@gmail.com>
2025-02-01 21:03:35 +0300

    refactor: replace router with folder route

---
+++ app/routes/home/route.tsx
@@ -0,0 +1,15 @@
+export default function Component() {
+  return (
+    <>
+      <ModeToggle />
+      <div>
+        {/* <Trans>Hello</Trans>; */}
+
+        <div className="m-10 p-10">          
+        </div>
+        {/* <ParentComponent/> */}
+        {/* <Button>Welcome To France</Button> */}
+      </div>
+    </>
+  );
+}
